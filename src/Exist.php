d1619937a2fbf75afe0db864d11b84abd75a2d2f
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-12-15 12:02:05 +0300

    First implementation

---
+++ src/Exist.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\Yii\ValidatorExistDb;
+
+use Closure;
+use Yiisoft\Validator\Rule\Trait\SkipOnEmptyTrait;
+use Yiisoft\Validator\Rule\Trait\SkipOnErrorTrait;
+use Yiisoft\Validator\Rule\Trait\WhenTrait;
+use Yiisoft\Validator\RuleWithOptionsInterface;
+use Yiisoft\Validator\SkipOnEmptyInterface;
+use Yiisoft\Validator\SkipOnErrorInterface;
+use Yiisoft\Validator\WhenInterface;
+
+final class Exist implements
+    RuleWithOptionsInterface,
+    SkipOnErrorInterface,
+    WhenInterface,
+    SkipOnEmptyInterface
+{
+    use SkipOnEmptyTrait;
+    use SkipOnErrorTrait;
+    use WhenTrait;
+
+    /**
+     * @var bool|callable|null
+     */
+    private $skipOnEmpty;
+
+    public function __construct(
+        private ?string $tableName = null,
+        private ?string $attribute = null,
+        private ?string $message = null,
+        private string $handlerClassName = ExistHandler::class,
+        bool|callable|null $skipOnEmpty = null,
+        private bool $skipOnError = false,
+        private Closure|null $when = null,
+    ) {
+        $this->skipOnEmpty = $skipOnEmpty;
+    }
+
+    public function getTableName(): ?string
+    {
+        return $this->tableName;
+    }
+
+    public function getAttribute(): ?string
+    {
+        return $this->attribute;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function getName(): string
+    {
+        return 'exist';
+    }
+
+    public function getHandlerClassName(): string
+    {
+        return $this->handlerClassName;
+    }
+
+    public function getOptions(): array
+    {
+        return [
+            'skipOnEmpty' => $this->getSkipOnEmptyOption(),
+            'skipOnError' => $this->skipOnError,
+        ];
+    }
+}
---
+++ src/ExistHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vjik\Yii\ValidatorExistDb;
+
+use Yiisoft\Db\Query\QueryInterface;
+use Yiisoft\Validator\Exception\UnexpectedRuleException;
+use Yiisoft\Validator\Result;
+use Yiisoft\Validator\RuleHandlerInterface;
+use Yiisoft\Validator\ValidationContext;
+
+final class ExistHandler implements RuleHandlerInterface
+{
+    public function __construct(
+        private QueryInterface $query,
+        private string $message = '{attribute} is invalid.',
+    ) {
+    }
+
+    public function validate(mixed $value, object $rule, ValidationContext $context): Result
+    {
+        if (!$rule instanceof Exist) {
+            throw new UnexpectedRuleException(Exist::class, $rule);
+        }
+
+        $exist = $this->query
+            ->from($rule->getTableName())
+            ->where([$rule->getAttribute() => $value])
+            ->exists();
+
+        $result = new Result();
+
+        if (!$exist) {
+            $result->addError(
+                $rule->getMessage() ?? $this->message,
+                [
+                    'attribute' => $context->getTranslatedAttribute(),
+                ]
+            );
+        }
+
+        return $result;
+    }
+}
